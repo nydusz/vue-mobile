1ad6d4335d2d1ad6ff15a2d550b06be7ea617b9a
nydusz <44310583+nydusz@users.noreply.github.com>
2021-07-04 19:15:59 +0800

    login-2

--- toutiao-m/src/main.js
+++ toutiao-m/src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 加载全局样式
-import './styles/index.less'
-
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
+// 加载全局样式 放到组件库后面
+import './styles/index.less'
+
 // 自动设置 REM 基准值
 import 'amfe-flexible'
 Vue.use(Vant)
